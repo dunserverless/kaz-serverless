53b66918b6460ac246928414d57c5b0217f77ca0
Karin Hawksworth <karin.hawksworth@dunelm.com>
2024-03-22 14:37:34 +0000

    with cognito user pool

--- cdk/main.js
+++ cdk/main.js
@@ -3,6 +3,7 @@
 const cdk = require('aws-cdk-lib')
 const { ApiStack } = require('./constructs/api-stack.js')
 const { DatabaseStack } = require('./constructs/database-stack.js')
+const { CognitoStack } = require('./constructs/cognito-stack')
 
 // pass value with context flag when you synth or deploy
 // cdk deploy --profile sand --context stageName=test
@@ -17,7 +18,10 @@ if (!stageName) {
 }
 
 const dbStack = new DatabaseStack(app, `DatabaseStack-${stageName}`, { stageName })
+const cognitoStack = new CognitoStack(app, `CognitoStack-${stageName}`, { stageName })
 new ApiStack(app, `ApiStack-${stageName}`, { 
   stageName,
   restaurantsTable: dbStack.restaurantsTable,
+  cognitoUserPool: cognitoStack.cognitoUserPool,
+  webUserPoolClient: cognitoStack.webUserPoolClient
 })
